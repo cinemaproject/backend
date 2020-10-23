847489bfcd8507f9bfb5c75e6e065701e2b117b7
Vladimir Kilyazov <west957@mail.ru>
2020-10-24 00:57:56 +0300

    init

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, render_template
+import insightface
+import urllib
+import urllib.request
+import cv2
+import numpy as np
+
+model = insightface.app.FaceAnalysis(rec_name = 'arcface_r100_v1', det_name = 'retinaface_mnet025_v2')
+ctx_id = -1
+model.prepare(ctx_id = ctx_id, nms=0.4)
+app = Flask(__name__)
+
+def url_to_image(url):
+    resp = urllib.request.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    return image
+
+@app.route('/')
+def hello():
+    return 'hello'
+
+
+@app.route('/actors_recognition', methods=['GET'])
+def recognize_actors():
+    img_url = request.args.get('url')
+    img = url_to_image(img_url)
+    img_out = img.copy()
+    faces = model.get(img)
+    for idx, face in enumerate(faces):
+        print("Face [%d]:"%idx)
+        print("\tage:%d"%(face.age))
+        gender = 'Male'
+        if face.gender==0:
+            gender = 'Female'
+        print("\tgender:%s"%(gender))
+        print("\tembedding shape:%s"%face.embedding.shape)
+        bboxes = face.bbox.astype(np.int)
+        print("\tbbox:%s"%(bboxes.flatten()))
+        img_out = cv2.rectangle(img_out, (bboxes[0], bboxes[1]), (bboxes[2], bboxes[3]), (0, 255, 0), 2)
+        print("\tlandmark:%s"%(face.landmark.astype(np.int).flatten()))
+        print("")
+        img_out = cv2.putText(img_out, str(face.age), (int(bboxes[0]-10), int(bboxes[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 255, 0),2)
+        img_out = cv2.putText(img_out, str(gender), (int(bboxes[0]+40), int(bboxes[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 0, 255),2)
+        img_out = cv2.putText(img_out, str(idx), (int(bboxes[0]-40), int(bboxes[1]-10)), cv2.FONT_HERSHEY_SIMPLEX, 1.1, (0, 0, 255),2)
+    cv2.imwrite('static\\result.jpg', img_out) 
+    return render_template("actors.html", res_image = 'static\\result.jpg')
+
+
+@app.route('/posters_by_color')
+def hello_name():
+    return "posters_by_color"
+
+
+if __name__ == '__main__':
+    app.run()
