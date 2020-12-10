42c1e665e55efa34ba3f8d6154a7cf78ce40a728
Alexander Batashev <alexbatashev@outlook.com>
2020-12-10 19:03:04 +0300

    Followup on #1

--- app/utils.py
+++ app/utils.py
@@ -4,12 +4,15 @@ import json
 
 def get_film_id(id):
     """Pad film ID to be 10 characters in length"""
-    return "{:<10}".format(id)
+    print(id)
+    # return id
+    return "{:<10}".format(str(id))
 
 
 def get_person_id(id):
     """Pad person ID to be 10 characters in length"""
-    return "{:<14}".format(id)
+    print(id)
+    return "{:<14}".format(str(id))
 
 
 def resultproxy_to_dict(resultproxy):
